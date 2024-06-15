7a8ebd5c0239c24b8cb1bed8465fbe1376a41339
Dmitry91-tec <nasdi91@yandex.ru>
2024-06-15 22:02:22 +1000

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+homework_completed = 12
+hour = 1.5
+name_course = 'Python'
+time_one = hour/homework_completed
+print('Курс:' + name_course + ',', 'всего задач:', homework_completed,'затрачено часов:',hour,', среднее время выполнения:',time_one,'часа')
